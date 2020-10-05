5f4407a5f557b63c4ed3509f2727ffac31d40dd4
Antonio Hrvoje Puđa <ahpudja@gmail.com.com>
2020-10-05 03:23:35 +0200

    rows placed inside wraper, txt and btn positioned

--- Js/JS.js
+++ Js/JS.js
@@ -1,6 +1,26 @@
-$(document).ready( function() {
+$(document).ready(function () {
+  $(".img").each(function (i) {
+    $(this).append("<img src='img/slider-image-" + ++i + ".jpg'/>");
+  });
+  /*Desni Gumb*/
+  $(".right").hover(function () {
+    $(".right").attr("src", "img/arrows/pd.png");
+  }); /* Promjena u plavo */
 
-    $(".img").each(function(i){
-    $(this).append("<img src='img/slider-image-"+(++i)+".jpg'/>");   
-    });
+  $(".right").mouseleave(function () {
+    $(".right").attr("src", "img/arrows/sd.png");
+  }); /*Vracanje u sivo */
+
+  /*Lijevi Gumb*/
+  $(".left").hover(function () {
+    $(".left").attr("src", "img/arrows/pl.png");
+  }); /* Promjena u plavo */
+
+  $(".left").mouseleave(function () {
+    $(".left").attr("src", "img/arrows/sl.png");
+  }); /*Vracanje u sivo */
+});
+
+$(document).ready( function(){
+console.log("ready")
 });
