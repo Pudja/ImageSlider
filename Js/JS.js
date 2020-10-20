c27734dd796217d649c32f92db1fc9e15b34e4a9
Antonio Hrvoje Puđa <ahpudja@gmail.com.com>
2020-10-20 22:37:45 +0200

    Logic Completed; need to decide on how to design it

--- Js/JS.js
+++ Js/JS.js
@@ -26,33 +26,41 @@ $(document).ready(function () {
   $(".right").click(function () {
     var lastFR = $(".my-img-row1 .img").first();
     var lastSR = $(".my-img-row2 .img").first();
-    $(".my-img-row1").animate({ right: lastFR.width() });
-    $(".my-img-row2").animate({ left: lastFR.width() });
-    $(".my-img-row2").append(lastFR);
-    $(".my-img-row1").append(lastSR);
+    var lastFRW =  lastFR.outerWidth();
+    var lastSRW = lastSR.outerWidth();
+    //FIRST ROW
+    $(".my-img-row1").animate({"right": "-="+lastFRW},
+    ()=>$(".my-img-row1").append(lastFR));
+    $(".my-img-row1").animate({"right": "+="+lastFRW},-2);
+
+    //SECOND ROW
+    $(".my-img-row2").animate({"right": "-="+lastSRW},
+    ()=>$(".my-img-row2").append(lastSR));
+    $(".my-img-row2").animate({"right": "+="+lastSRW},-2);
   });
+
   /*left button logic */
   $(".left").click(function () {
-    sumFR = 0;
-    sumSR = 0;
-    resetFR = 0;
-    resetSR = 0;
     var firstFR = $(".my-img-row1 .img").last();
     var firstSR = $(".my-img-row2 .img").last();
     var firstFRW = firstFR.outerWidth();
     var firstSRW = firstSR.outerWidth();
 
-    sumFR = resetFR + firstFRW;
-    resetFR = sumFR * 0;
-    sumSR = resetSR - firstSRW;
-    resetSR = sumSR * 0;
+    //FIRST ROW
+    $(".my-img-row1").animate({"right": "+="+firstFRW},
+    ()=>$(".my-img-row1").prepend(firstFR));
+    $(".my-img-row1").animate({"right": "-="+firstFRW},-2);
 
-    $(".my-img-row1").animate({ right: sumFR },200, 
-      () => $(".my-img-row1").prepend(firstSR));
-      //() => $(".my-img-row1").animate({ right: resetFR },200) );
-    $(".my-img-row2").animate({ right: sumSR }, 200,
-      () => $(".my-img-row2").prepend(firstFR));
-      //() => $(".my-img-row2").animate({ right: resetSR },200));
+    //SECOND ROW
+    $(".my-img-row2").animate({"right": "+="+firstSRW},
+    ()=>$(".my-img-row2").prepend(firstSR));
+    $(".my-img-row2").animate({"right": "-="+firstSRW},-2);
   });
 });
-
+/*
+    var firstFRW = firstFR.outerWidth();
+    var firstSRW = firstSR.outerWidth();
+    var txtW = $(".my-txt-area").outerWidth()
+    var lastFR = row1.first();
+    var lastSR = row2.first();
+*/
